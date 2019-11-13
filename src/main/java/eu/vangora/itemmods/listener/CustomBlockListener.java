313d63a186bca73f9d2ad8bb27b6c56b2eefdbcd
CodeDoctorDE <wusel905@gmail.com>
2019-11-13 08:28:15 +0100

    fix multi break bug

--- src/main/java/eu/vangora/itemmods/listener/CustomBlockListener.java
+++ src/main/java/eu/vangora/itemmods/listener/CustomBlockListener.java
@@ -2,8 +2,10 @@ package eu.vangora.itemmods.listener;
 
 import eu.vangora.itemmods.config.BlockConfig;
 import eu.vangora.itemmods.main.Main;
+import org.bukkit.Bukkit;
 import org.bukkit.Location;
 import org.bukkit.Material;
+import org.bukkit.block.Block;
 import org.bukkit.entity.ArmorStand;
 import org.bukkit.entity.Entity;
 import org.bukkit.entity.EntityType;
@@ -12,6 +14,9 @@ import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
 import org.bukkit.event.block.Action;
 import org.bukkit.event.block.BlockBreakEvent;
+import org.bukkit.event.block.BlockFromToEvent;
+import org.bukkit.event.block.BlockPistonExtendEvent;
+import org.bukkit.event.entity.EntityChangeBlockEvent;
 import org.bukkit.event.player.PlayerInteractEvent;
 import org.bukkit.inventory.EntityEquipment;
 
@@ -52,7 +57,7 @@ public class CustomBlockListener implements Listener {
                         location.add(0, 0, 1);
                         break;
                 }
-                if (location.getNearbyPlayers(1).contains(event.getPlayer())) return;
+                if (location.getNearbyPlayers(0.5).contains(event.getPlayer())) return;
                 if (!location.getBlock().isEmpty() || location.getBlock().getState().equals(block.getBlock())) return;
                 location.getWorld().getBlockAt(location).setBlockData(block.getBlock());
                 ArmorStand armorStand = (ArmorStand) location.getWorld().spawnEntity(location.add(0.5, 0, 0.5), EntityType.ARMOR_STAND);
@@ -81,13 +86,51 @@ public class CustomBlockListener implements Listener {
 
     @EventHandler
     public void onCustomBlockBreak(BlockBreakEvent event) {
+        Location location = event.getBlock().getLocation().add(0.5, 0, 0.5);
+        List<Entity> entities = new ArrayList<>(location.getNearbyEntitiesByType(ArmorStand.class, 0.01, 0.001, 0.01));
+        Bukkit.broadcastMessage(entities.toString() + location.toString());
+        if (entities.size() < 1) return;
+        Main.getPlugin().getMainConfig().getBlocks().forEach(block -> entities.stream().filter(entity -> entity.getType() == EntityType.ARMOR_STAND).map(entity -> (ArmorStand) entity).filter(armorStand -> armorStand.getScoreboardTags().contains(block.getTag())).forEach(armorStand -> {
+            if (location.distance(armorStand.getLocation()) <= 0.5) {
+                event.setCancelled(true);
+                event.getPlayer().sendMessage(armorStand.getLocation().toString());
+                event.getBlock().setType(Material.AIR);
+                event.getBlock().getLocation().getWorld().dropItem(event.getBlock().getLocation(), block.getItemStack());
+                armorStand.remove();
+            }
+        }));
+    }
+
+    @EventHandler
+    public void onCustomBlockMove(BlockFromToEvent event) {
+        Bukkit.broadcastMessage("test");
+        List<Entity> entities = new ArrayList<>(event.getBlock().getLocation().add(0.5, 0.1, 0.5).getNearbyLivingEntities(0.05, 0.001));
+        if (entities.size() < 1) return;
+        Main.getPlugin().getMainConfig().getBlocks().forEach(block -> entities.stream().filter(entity -> entity.getType() == EntityType.ARMOR_STAND).map(entity -> (ArmorStand) entity).filter(armorStand -> armorStand.getScoreboardTags().contains(block.getTag())).forEach(armorStand -> {
+            armorStand.teleport(event.getToBlock().getLocation());
+        }));
+    }
+
+    @EventHandler
+    public void onCustomBlockPistonMove(BlockPistonExtendEvent event) {
+        for (Block block :
+                event.getBlocks()) {
+            Bukkit.broadcastMessage(block.getLocation().toString());
+            List<Entity> entities = new ArrayList<>(block.getLocation().add(0.5, 0.1, 0.5).getNearbyLivingEntities(0.05, 0.001));
+            if (entities.size() < 1) return;
+            Main.getPlugin().getMainConfig().getBlocks().forEach(blockConfig -> entities.stream().filter(entity -> entity.getType() == EntityType.ARMOR_STAND).map(entity -> (ArmorStand) entity).filter(armorStand -> armorStand.getScoreboardTags().contains(blockConfig.getTag())).forEach(armorStand -> {
+                Bukkit.broadcastMessage(armorStand.getUniqueId().toString());
+                armorStand.teleport(block.getLocation().add(event.getDirection().getDirection()).add(0.5, 0, 0.5));
+            }));
+        }
+    }
+
+    @EventHandler
+    public void onCustomBlockFall(EntityChangeBlockEvent event) {
         List<Entity> entities = new ArrayList<>(event.getBlock().getLocation().add(0.5, 0, 0.5).getNearbyLivingEntities(0.1, 0.01));
         if (entities.size() < 1) return;
         Main.getPlugin().getMainConfig().getBlocks().forEach(block -> entities.stream().filter(entity -> entity.getType() == EntityType.ARMOR_STAND).map(entity -> (ArmorStand) entity).filter(armorStand -> armorStand.getScoreboardTags().contains(block.getTag())).forEach(armorStand -> {
             event.setCancelled(true);
-            event.getBlock().setType(Material.AIR);
-            event.getBlock().getLocation().getWorld().dropItem(event.getBlock().getLocation(), block.getItemStack());
-            armorStand.remove();
         }));
     }
 }
