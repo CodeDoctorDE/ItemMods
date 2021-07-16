8a64b195642d93fab93119e215901f12ddff9e04
CodeDoctorDE <wusel905@gmail.com>
2021-07-16 09:38:06 +0200

    designing main gui better

--- src/main/java/com/github/codedoctorde/itemmods/gui/KnowledgeGui.java
+++ src/main/java/com/github/codedoctorde/itemmods/gui/KnowledgeGui.java
@@ -8,7 +8,7 @@ import org.bukkit.Material;
 
 public class KnowledgeGui extends TranslatedChestGui {
     public KnowledgeGui() {
-        super(ItemMods.getTranslationConfig().subTranslation("gui.knowledge"));
+        super(ItemMods.getTranslationConfig().subTranslation("gui.knowledge"), 4);
         //Translation t = getTranslation();
         registerItem(4, 1, new TranslatedItem(ItemMods.getTranslationConfig().getInstance(), new ItemStackBuilder(Material.BARRIER).setDisplayName("coming-soon").build()));
     }
--- src/main/java/com/github/codedoctorde/itemmods/gui/MainGui.java
+++ src/main/java/com/github/codedoctorde/itemmods/gui/MainGui.java
@@ -13,9 +13,11 @@ import org.bukkit.entity.Player;
 
 public class MainGui extends TranslatedChestGui {
     public MainGui() {
-        super(ItemMods.getTranslationConfig().subTranslation("gui.main"));
+        super(ItemMods.getTranslationConfig().subTranslation("gui.main"), 4);
         Translation translation = getTranslation();
         setPlaceholders(ItemMods.getVersion());
+        fillItems(0, 0, 0, 3, new StaticItem(new ItemStackBuilder(Material.BLACK_STAINED_GLASS_PANE).build()));
+        fillItems(8, 0, 8, 3, new StaticItem(new ItemStackBuilder(Material.BLACK_STAINED_GLASS_PANE).build()));
         fillItems(0, 0, getWidth() - 1, 0, new StaticItem(new ItemStackBuilder(Material.BLACK_STAINED_GLASS_PANE).setDisplayName(" ").build()));
         fillItems(0, getHeight() - 1, getWidth() - 1, getHeight() - 1, new StaticItem(new ItemStackBuilder(Material.BLACK_STAINED_GLASS_PANE).setDisplayName(" ").build()));
         addItem(new TranslatedGuiItem(new ItemStackBuilder(Material.PRISMARINE_CRYSTALS).setDisplayName("reload.title").addLore("reload.description").build()) {{
@@ -29,8 +31,7 @@ public class MainGui extends TranslatedChestGui {
             setClickAction(event -> new PacksGui().show((Player) event.getWhoClicked()));
         }});
         addItem(new TranslatedGuiItem(new ItemStackBuilder(Material.KNOWLEDGE_BOOK).setDisplayName("knowledge.title").addLore("knowledge.description").build()) {{
-            setClickAction(inventoryClickEvent -> new MaterialListGui(new Translation(), (material)->{}).show((Player) inventoryClickEvent.getWhoClicked()));
-            //setClickAction(event -> new KnowledgeGui().show((Player) event.getWhoClicked()));
+            setClickAction(event -> new KnowledgeGui().show((Player) event.getWhoClicked()));
         }});
         addItem(new TranslatedGuiItem(new ItemStackBuilder(Material.PAPER).setDisplayName("source.title").addLore("source.description").build()) {{
             setClickAction(event -> event.getWhoClicked().sendMessage(translation.getTranslation("source.link")));
@@ -41,7 +42,7 @@ public class MainGui extends TranslatedChestGui {
         addItem(new TranslatedGuiItem(new ItemStackBuilder(Material.BOOK).setDisplayName("wiki.title").setLore("wiki.description").build()) {{
             setClickAction(event -> event.getWhoClicked().sendMessage(translation.getTranslation("wiki.link")));
         }});
-        addItem(new TranslatedGuiItem(new ItemStackBuilder(Material.BOOK).setDisplayName("crowdin.title").setLore("crowdin.description").build()) {{
+        addItem(new TranslatedGuiItem(new ItemStackBuilder(Material.PAINTING).setDisplayName("crowdin.title").setLore("crowdin.description").build()) {{
             setClickAction(event -> event.getWhoClicked().sendMessage(translation.getTranslation("crowdin.link")));
         }});
     }
