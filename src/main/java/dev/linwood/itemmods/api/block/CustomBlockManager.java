dfa595aabd93e7f0ec949495cd813ccb0e6c6ed0
CodeDoctor <wusel905@mail.com>
2021-09-02 17:29:58 +0200

    fix nbt api

--- src/main/java/dev/linwood/itemmods/api/block/CustomBlockManager.java
+++ src/main/java/dev/linwood/itemmods/api/block/CustomBlockManager.java
@@ -19,12 +19,14 @@ import java.util.Objects;
 
 public class CustomBlockManager {
 
-    public static @NotNull String locationToString(final @Nullable Location location) {
+    public static @NotNull
+    String locationToString(final @Nullable Location location) {
         if (location == null) return "";
         return Objects.requireNonNull(location.getWorld()).getName() + ":" + location.getBlockX() + ":" + location.getBlockY() + ":" + location.getBlockZ();
     }
 
-    public static @NotNull Location stringToLocation(final @NotNull String location) {
+    public static @NotNull
+    Location stringToLocation(final @NotNull String location) {
         String[] locationArray = location.split(":");
         return new Location(Bukkit.getWorld(locationArray[0]), Double.parseDouble(locationArray[1]),
                 Double.parseDouble(locationArray[2]),
@@ -45,7 +47,7 @@ public class CustomBlockManager {
         if (location.getBlock().getType().isSolid() || asset == null)
             return null;
         var model = asset.getModel();
-        if(model == null || model.getFallbackTexture() == null)
+        if (model == null || model.getFallbackTexture() == null)
             return null;
         var event = new CustomBlockPlaceEvent(location, packObject, player);
         Bukkit.getPluginManager().callEvent(event);
@@ -53,22 +55,16 @@ public class CustomBlockManager {
             return null;
         Block block = Objects.requireNonNull(location.getWorld()).getBlockAt(location);
         block.setType(Material.SPAWNER);
-        ((TileState)block.getState()).getPersistentDataContainer().set(CustomBlock.TYPE_KEY, PersistentDataType.STRING, packObject.toString());
-            NBTTileEntity tent = new NBTTileEntity(block.getState());
-            tent.setInteger("RequiredPlayerRange", 0);
-            var spawnData = tent.addCompound("SpawnData");
-            spawnData.setString("id", EntityType.ARMOR_STAND.getKey().toString());
-            var armorItems = spawnData.getCompoundList("ArmorItems");
-            // Boots
-            armorItems.addCompound();
-            // Leggings
-            armorItems.addCompound();
-            // Chest plate
-            armorItems.addCompound();
-            // Head
-            var head = armorItems.addCompound();
-            head.setString("id", model.getFallbackTexture().getKey().toString());
-            head.setDouble("Count", 1d);
+        ((TileState) block.getState()).getPersistentDataContainer().set(CustomBlock.TYPE_KEY, PersistentDataType.STRING, packObject.toString());
+        NBTTileEntity tent = new NBTTileEntity(block.getState());
+        tent.setInteger("RequiredPlayerRange", 0);
+        var spawnData = tent.addCompound("SpawnData");
+        spawnData.setString("id", EntityType.ARMOR_STAND.getKey().toString());
+        var handItems = spawnData.getCompoundList("HandItems");
+        var hand = handItems.addCompound();
+        hand.setString("id", model.getFallbackTexture().getKey().toString());
+        hand.addCompound("tag").setInteger("CustomModelData", asset.getModelObject() == null ? null : asset.getModelObject().getCustomModel());
+        hand.setDouble("Count", 1d);
 
         return customBlock;
     }
