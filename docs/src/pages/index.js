fb547a39895151bd02297fb4b329f98a08a631c9
CodeDoctor <wusel905@mail.com>
2021-09-05 10:34:12 +0200

    remove homepage features

--- docs/src/pages/index.js
+++ docs/src/pages/index.js
@@ -4,7 +4,6 @@ import Layout from '@theme/Layout';
 import Link from '@docusaurus/Link';
 import useDocusaurusContext from '@docusaurus/useDocusaurusContext';
 import styles from './index.module.css';
-import HomepageFeatures from '../components/HomepageFeatures';
 
 function HomepageHeader() {
   const {siteConfig} = useDocusaurusContext();
@@ -17,7 +16,7 @@ function HomepageHeader() {
           <Link
             className="button button--secondary button--lg"
             to="/docs">
-            Docusaurus Tutorial - 5min ⏱️
+            Docs
           </Link>
         </div>
       </div>
@@ -32,9 +31,6 @@ export default function Home() {
       title={`Hello from ${siteConfig.title}`}
       description="Add custom items/blocks to your game.">
       <HomepageHeader />
-      <main>
-        <HomepageFeatures />
-      </main>
     </Layout>
   );
 }
